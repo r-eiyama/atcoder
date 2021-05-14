946acbb5cac7c4ce96a9a296a2a4d04276af3a2a
yamee-dev <ryo.eiyama@icloud.com>
2021-05-14 23:01:38 +0900

    問題といた

---
+++ tutorial2/ex18.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(M), B(M);
+    for (int i = 0; i < M; i++)
+    {
+        cin >> A.at(i) >> B.at(i);
+    }
+
+    // ここにプログラムを追記
+    // (ここで"試合結果の表"の2次元配列を宣言)
+    vector<vector<char>> result(N, vector<char>(N, '-'));
+
+    for (int i = 0; i < M; i++)
+    {
+        A.at(i)--;
+        B.at(i)--;
+        result.at(A.at(i)).at(B.at(i)) = 'o'; // AはBに勝った
+        result.at(B.at(i)).at(A.at(i)) = 'x'; // BはAに負けた
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << result.at(i).at(j);
+            if (j == N - 1)
+            {
+                cout << endl;
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+    }
+}
