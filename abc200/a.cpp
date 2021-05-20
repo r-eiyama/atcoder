847fe77bf76c73975dbbda003bd3b5bfc5b041e4
yamee-dev <ryo.eiyama@icloud.com>
2021-05-20 23:47:49 +0900

    問題といた

---
+++ abc200/a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << floor((n + 99) / 100) << endl;
+}
