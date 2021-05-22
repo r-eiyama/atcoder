4bfc455516e08d2ede4c2d9ad76d600d3463ae7a
yamee-dev <ryo.eiyama@icloud.com>
2021-05-22 21:25:11 +0900

    コミット

---
+++ abc199/b.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    vector<int> b(n);
+    rep(i, n) cin >> a.at(i);
+    rep(j, n) cin >> b.at(j);
+
+    vector<int> cnt(1000);
+}
